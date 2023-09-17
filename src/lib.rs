d33434eb69354b8b6f7e32114fe2bc44a292d391
Command <me@c0mm4nd.com>
2023-09-17 15:55:30 +0800

    remove unnecessary protocol namespcae

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,1 @@
-
-pub mod protocol {
-    // tonic::include_proto!("google.api");
-    tonic::include_proto!("protocol"); // The string specified here must match the proto package name
-}
+tonic::include_proto!("protocol");
