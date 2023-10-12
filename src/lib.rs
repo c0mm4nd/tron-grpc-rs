b2c3d351f94d9bba61c0eec589a5b95f6e802308
Command <me@c0mm4nd.com>
2023-10-13 03:26:16 +0800

    add impl Name for known contract calls

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,169 @@
 tonic::include_proto!("protocol");
+
+impl prost::Name for AccountCreateContract {
+    const NAME: &'static str = "TransferAssetContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for TransferContract {
+    const NAME: &'static str = "TransferContract";
+    const PACKAGE: &'static str = "protocol";
+}
+
+impl prost::Name for TransferAssetContract {
+    const NAME: &'static str = "TransferAssetContract";
+    const PACKAGE: &'static str = "protocol";
+}
+
+// impl prost::Name for VoteAssetContract {
+//     const NAME: &'static str = "VoteAssetContract";
+//     const PACKAGE: &'static str = "protocol";
+// }
+
+impl prost::Name for VoteWitnessContract {
+    const NAME: &'static str = "VoteWitnessContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for WitnessCreateContract {
+    const NAME: &'static str = "WitnessCreateContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for AssetIssueContract {
+    const NAME: &'static str = "AssetIssueContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for WitnessUpdateContract {
+    const NAME: &'static str = "WitnessUpdateContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for ParticipateAssetIssueContract {
+    const NAME: &'static str = "ParticipateAssetIssueContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for AccountUpdateContract {
+    const NAME: &'static str = "AccountUpdateContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for FreezeBalanceContract {
+    const NAME: &'static str = "FreezeBalanceContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for UnfreezeBalanceContract {
+    const NAME: &'static str = "UnfreezeBalanceContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for WithdrawBalanceContract {
+    const NAME: &'static str = "WithdrawBalanceContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for UnfreezeAssetContract {
+    const NAME: &'static str = "UnfreezeAssetContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for UpdateAssetContract {
+    const NAME: &'static str = "UpdateAssetContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for ProposalCreateContract {
+    const NAME: &'static str = "ProposalCreateContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for ProposalApproveContract {
+    const NAME: &'static str = "ProposalApproveContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for ProposalDeleteContract {
+    const NAME: &'static str = "ProposalDeleteContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for SetAccountIdContract {
+    const NAME: &'static str = "SetAccountIdContract";
+    const PACKAGE: &'static str = "protocol";
+}
+// impl prost::Name for crate::transaction::contract::ContractType {
+//     const NAME: &'static str = "CustomContract";
+//     const PACKAGE: &'static str = "protocol";
+// }
+impl prost::Name for CreateSmartContract {
+    const NAME: &'static str = "CreateSmartContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for TriggerSmartContract {
+    const NAME: &'static str = "TriggerSmartContract";
+    const PACKAGE: &'static str = "protocol";
+}
+// impl prost::Name for ContractType {
+//     const NAME: &'static str = "GetContract";
+//     const PACKAGE: &'static str = "protocol";
+// }
+impl prost::Name for UpdateSettingContract {
+    const NAME: &'static str = "UpdateSettingContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for ExchangeCreateContract {
+    const NAME: &'static str = "ExchangeCreateContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for ExchangeInjectContract {
+    const NAME: &'static str = "ExchangeInjectContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for ExchangeWithdrawContract {
+    const NAME: &'static str = "ExchangeWithdrawContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for ExchangeTransactionContract {
+    const NAME: &'static str = "ExchangeTransactionContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for UpdateEnergyLimitContract {
+    const NAME: &'static str = "UpdateEnergyLimitContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for AccountPermissionUpdateContract {
+    const NAME: &'static str = "AccountPermissionUpdateContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for ClearAbiContract {
+    const NAME: &'static str = "ClearABIContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for UpdateBrokerageContract {
+    const NAME: &'static str = "UpdateBrokerageContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for ShieldedTransferContract {
+    const NAME: &'static str = "ShieldedTransferContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for MarketSellAssetContract {
+    const NAME: &'static str = "MarketSellAssetContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for MarketCancelOrderContract {
+    const NAME: &'static str = "MarketCancelOrderContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for FreezeBalanceV2Contract {
+    const NAME: &'static str = "FreezeBalanceV2Contract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for UnfreezeBalanceV2Contract {
+    const NAME: &'static str = "UnfreezeBalanceV2Contract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for WithdrawExpireUnfreezeContract {
+    const NAME: &'static str = "WithdrawExpireUnfreezeContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for DelegateResourceContract {
+    const NAME: &'static str = "DelegateResourceContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for UnDelegateResourceContract {
+    const NAME: &'static str = "UnDelegateResourceContract";
+    const PACKAGE: &'static str = "protocol";
+}
+impl prost::Name for CancelAllUnfreezeV2Contract {
+    const NAME: &'static str = "CancelAllUnfreezeV2Contract";
+    const PACKAGE: &'static str = "protocol";
+}
